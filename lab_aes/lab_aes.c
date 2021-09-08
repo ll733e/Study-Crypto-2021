020bcd8e3224b6877b1752bd9a69062b92e33f37
iphone <purplesky@korea.ac.kr>
2021-09-08 20:03:19 +0900

    초기 커밋.

---
+++ lab_aes/lab_aes.c
@@ -0,0 +1,25 @@
+﻿#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab_aes.h"
+
+#define MUL2(a) (a<<1)^(a & 0x80 ? 0x1b : 0)
+#define MUL3(a) (MUL2(a))^(a)
+#define MUL4(a) (MUL2((MUL2(A))))
+#define MUL8(a) MUL2((MUL2((MUL2(a)))))
+#define MUL9(a)
+#define MULB(a)
+#define MULD(a)
+#define MULE(a)
+
+int main()
+{
+    u8 a, b;
+
+    a = 0xab;
+    b = MUL2(a);
+
+    printf("MUL2(%02x)=%02x\n", a, b);
+
+    return 0;
+}
