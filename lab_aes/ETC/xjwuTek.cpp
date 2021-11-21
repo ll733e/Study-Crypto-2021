5b8f64b4f161df2a8a44b2c75687197a8e3575c2
iphone <73981769+louxsoen@users.noreply.github.com>
2021-11-22 01:13:09 +0900

    Stack-Based Buffer Overflow Information Disclosure Vulnerability #1 FIXED
    우린 이런걸 재능낭비라고 부르기로 했어요
    
    Co-Authored-By: DoHyun Lee <43539720+dlehgus1023@users.noreply.github.com>

--- lab_aes/ETC/xjwuTek.cpp
+++ lab_aes/ETC/xjwuTek.cpp
@@ -2,18 +2,17 @@
 #include <string>
 using namespace std;
 
-// AES, Key size - 128bit;
+
 int main() 
 {
     string pl;  // 평문
 
     int width, height;
-    string *p = &pl;
     cout << "평문 입력 >> ";
     cin >> pl;
     
     int pl_len = pl.length();
     
     for(int re = 0 ; re < pl_len ; re++)
-    cout << dec << p[re] << " ";
+    cout << dec << pl[re] << " ";
 }
